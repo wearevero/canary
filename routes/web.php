bf3146e4610ec3b6d99b0f32191bbfcd93a9d8df
yuxxeun <itsyuxxeun@gmail.com>
2023-02-17 15:58:57 +0700

    vendor: add fully spatie health checker deps

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,12 @@ use Illuminate\Support\Facades\Route;
 use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::get('health', HealthCheckResultsController::class);
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
